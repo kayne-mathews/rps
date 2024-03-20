4d3c007dd16e9a6a149fabe9e4e3d74a549f188b
kayn-mathews <160433017+kayne-mathews@users.noreply.github.com>
2024-03-20 00:35:23 -0400

    FINISHED
    Code was so bad before I didnt bother commiting

--- index.js
+++ index.js
@@ -1,14 +1,79 @@
+console.log("Welcome to Rock, Paper, Scissors!")
+console.log("")
+
 function getComputerChoice() {
- result = (Math.floor(Math.random() * 3 + 1))
- if (result == 1) {
-    return "rock"
- } else if ( result == 2 ) {
-     return "paper";
- } else {
-     return "scissors";
- }
-};
+    let number = (Math.floor(Math.random() * 3 + 1));
+      if (number == 1) {
+      return "rock";
+    } else if (number == 2) {
+      return "paper";
+    } else {
+      return "scissors";    
+    }
+  }
 
+//starting player and computer score
+  
+let playerScore = 0;
+let computerScore = 0;
 
+//function to prompt for player choice
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+return prompt("Choose 'Rock' 'Paper' or 'Scissors'", ).toLowerCase();
+}
+
+//function to play one round
+
+function playRound (computerSelection, playerSelection) {
+  if (playerSelection == computerSelection) {
+  return "Tie Round!";
+} else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+  return "You won that round!";
+} else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
+  return "You lost that round!";
+} else {
+  return "Im not sure what you entered but it needs to be 'Rock' 'Paper' or 'Scissors'";
+}
+}
+
+//function to play game multiple times
+
+function playGame() {
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayerChoice();
+  console.log("Player Chose " + playerSelection);
+  console.log("Computer Chose " + computerSelection);
+  console.log(playRound(computerSelection,playerSelection));
+  if (playRound(computerSelection,playerSelection) == "You won that round!") {
+    ++playerScore;
+} else if (playRound(computerSelection,playerSelection) == "You lost that round!") {
+    ++computerScore;
+}
+  console.log("player score is " + playerScore + " computer score is " + computerScore);
+  console.log(" ");
+}
+
+//play game 5 times
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
+
+//function to print winner
+
+function winner() {
+  if (playerScore > computerScore) {
+    console.log("You have won!");
+} else if (computerScore > playerScore) {
+    console.log("Sorry the computer has won!");
+} else {
+    console.log("No winner.");
+}
+}
+
+//print winner
+
+winner();
